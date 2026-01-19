4d284372f8466015cb5525f3954b8866af4a6b9d
bpmryan <bpm_ryand2428@yahoo.com>
2026-01-18 22:12:38 -0600

    Question 12 to ml1 done

--- ml1/hw1/code/frequency.py
+++ ml1/hw1/code/frequency.py
@@ -4,4 +4,20 @@ url = 'https://www.gutenberg.org/files/20727/20727.txt'
 with get(url) as response:
     text = response.read().decode('utf-8')
 
-print(text)
+# print(text)
+count = {}
+allWords = []
+
+# reads txt as a list and counts each word until the end
+for word in text.split(" "):
+    # checks the length of the word and counts the words greater than 5 char
+    if len(word) > 5:
+        if word not in count:
+            count.update({word: 1})
+            allWords.append(word)
+        else: 
+            count[word] += 1
+
+# loop that gets the 5 most sorted words
+for word in sorted(count, key = count.get, reverse = True)[0:10]: 
+    print(word, ": ", count[word])
