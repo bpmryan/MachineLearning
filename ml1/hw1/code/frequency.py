bf8d1f06a71ec0c763b83d547241922586f05378
bpmryan <bpm_ryand2428@yahoo.com>
2026-01-21 22:18:11 -0600

    Revise solution

--- ml1/hw1/code/frequency.py
+++ ml1/hw1/code/frequency.py
@@ -5,19 +5,25 @@ with get(url) as response:
     text = response.read().decode('utf-8')
 
 # print(text)
+# puts all words to lowercase
+text = text.lower()
+
+# remove punctuation 
+punctuation = ".,!?;:\"()[]{}<>-_—'*“”’"
+for p in punctuation:
+    text = text.replace(p, " ")
+
 count = {}
-allWords = []
 
 # reads txt as a list and counts each word until the end
-for word in text.split(" "):
+for word in text.split():
     # checks the length of the word and counts the words greater than 5 char
     if len(word) > 5:
         if word not in count:
-            count.update({word: 1})
-            allWords.append(word)
-        else: 
+            count[word] = 1
+        else:
             count[word] += 1
 
 # loop that gets the 5 most sorted words
-for word in sorted(count, key = count.get, reverse = True)[0:10]: 
-    print(word, ": ", count[word])
+for word in sorted(count, key = count.get, reverse = True)[:10]: 
+    print(word, ":", count[word])
