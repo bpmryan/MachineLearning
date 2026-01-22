765e0b4e84a4d3248043c3ee6c3206f2dfcb8dad
bpmryan <bpm_ryand2428@yahoo.com>
2026-01-22 16:18:08 -0600

    Create chessboard

--- ml1/hw2/code/ChessBoard.py
+++ ml1/hw2/code/ChessBoard.py
@@ -1,3 +1,14 @@
 # question 3
 from matplotlib import pyplot as plt
 import numpy as np
+
+X = np.arange(8*8).reshape(8, 8) % 2
+for i in range(0, X.shape[0], 2):
+    X[i] = (X[i] + 1) % 2
+
+plt.title('Chess Board Pattern')
+plt.imshow(X, cmap='grey')
+plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+plt.yticks([0, 1, 2, 3, 4, 5, 6, 7], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+plt.tight_layout()
+plt.show()
