71c4225b3bdd7e8c8e8dae982108fb726e6b2599
bpmryan <bpm_ryand2428@yahoo.com>
2026-02-09 15:40:47 -0600

    Adjust dinh.py

--- ml1/hw3/code/dinh.py
+++ ml1/hw3/code/dinh.py
@@ -1,16 +1,11 @@
 import numpy as np 
-from data import X, y 
+from data import X, y, X_, y_
 from bayes import NaiveBayes
 
 model = NaiveBayes(X, y)
-
-# np.array([class, sex, age, fare (in lbs)])
-testSubject = np.array([3, 1, 20, 20])
-
-prediction = model.predict(testSubject)
-
-if prediction == 1:
-    print("You survived")
-else:
-    print("You've somehow went further than 6ft under. Whomp whomp o7")
-    print("Better luck next time")
+if __name__ == '__main__':
+    # class, sex, age, fare
+    print(model.predict(np.array([3, 1, 20, 2])))
+    preds = [model.predict(x) for x in X_]
+    accuracy = np.sum(preds == y_) / y_.shape[0]
+    print(f'Accuracy: {accuracy}')
