6bac62b14a794088df13e5afce71655b1249cb2c
bpmryan <bpm_ryand2428@gmail.com>
2026-02-18 23:31:54 -0600

    Look back at 4.1

--- ml1/hw4/code/regression.py
+++ ml1/hw4/code/regression.py
@@ -30,30 +30,30 @@ titles = [
 
 # print(Xy[:,-1].min(), Xy[:,-1].mean(), Xy[:,-1].max())
 
-# for i in range(Xy.shape[1]):
-#     abbrv = titles[i].split('   ')[0]
-#     label = titles[i].split('   ')[-1]
-#     print(abbrv, label)
-#     plt.title(abbrv)
-#     plt.xlabel(label)
-#     plt.ylabel('Median value of over-occupied homes in $1000\'s')
-#     plt.plot(Xy[:,i], Xy[:,-1], 'ko', alpha=0.5)
-#     plt.tight_layout()
-#     plt.show()
+for i in range(Xy.shape[1]):
+    abbrv = titles[i].split('   ')[0]
+    label = titles[i].split('   ')[-1]
+    print(abbrv, label)
+    plt.title(abbrv)
+    plt.xlabel(label)
+    plt.ylabel('Median value of over-occupied homes in $1000\'s')
+    plt.plot(Xy[:,i], Xy[:,-1], 'ko', alpha=0.5)
+    plt.tight_layout()
+    plt.show()
 
 # Question 4.1 RM (column 5) and LSTAT (column 12) are the most correlated.
-Xy = np.column_stack((Xy[:, 5], Xy[:, 12], np.ones(shape=Xy.shape[0]), Xy[:, -1]))
-print(Xy[:20])
-test = Xy[-100:]
-train = Xy[:-100]
-X, y = train[:, :-1], train[:, -1]
-_X, _y = test[:, :-1], test[:, -1]
+# Xy = np.column_stack((Xy[:, 5], Xy[:, 12], np.ones(shape=Xy.shape[0]), Xy[:, -1]))
+# print(Xy[:20])
+# test = Xy[-100:]
+# train = Xy[:-100]
+# X, y = train[:, :-1], train[:, -1]
+# _X, _y = test[:, :-1], test[:, -1]
 
 # 4.2 
 # linear regression model
-beta = np.linalg.inv(X.T @ X) @ X.T @ y
+# beta = np.linalg.inv(X.T @ X) @ X.T @ y
 
 # root mean squared error (rmse) 
-J = (_X @ beta - _y).T @ (_X @ beta - _y)
-rmse = np.sqrt(J / _X.shape[0])
-print(rmse)
+# J = (_X @ beta - _y).T @ (_X @ beta - _y)
+# rmse = np.sqrt(J / _X.shape[0])
+# print(rmse)
