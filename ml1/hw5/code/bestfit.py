0cc8515b0e5c53a798c8c7942fda1ccdfc725d1f
bpmryan <bpm_ryand2428@gmail.com>
2026-03-25 15:22:23 -0500

    add files from hw4

---
+++ ml1/hw5/code/bestfit.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+mean = [0, 0]
+cov = [[1, 0.9], 
+       [0.9, 1]]
+
+rng = np.random.default_rng(0)
+X, y = rng.multivariate_normal(mean, cov, 100).T
+
+X = np.vstack((X, np.ones(shape=X.shape))).T
+
+beta = np.linalg.inv(X.T @ X) @ (X.T @ y)
+m, b = beta
+
+a = np.linspace(X.min(), X.max())
+c = m*a + b
+
+plt.title('Linear Regression in Two Dimensions')
+plt.plot(X[:, 0], y, 'ko', alpha=0.5, label='Data')
+plt.plot(a, c, 'b.-', label='Line of Bestfit')
+plt.legend()
+plt.tight_layout()
+
+plt.show()
---
+++ ml1/hw5/code/regression.py
@@ -0,0 +1,59 @@
+import numpy as np
+from matplotlib import pyplot as plt 
+
+PATH = '../media/BostonHousing.csv'
+
+Xy = np.genfromtxt(
+    PATH, 
+    delimiter=',',
+    skip_header=1,
+    dtype=np.float64,
+    converters = {3: lambda s: float(s[1: -1])}
+)
+
+titles = [
+    'CRIM     per capita crime rate by town',
+    'ZN       proportion of residential land zoned for lots over 25,000 sq.ft.',
+    'INDUS    proportion of non-retail business acres per town',
+    'CHAS     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)',
+    'NOX      nitric oxides concentration (parts per 10 million)',
+    'RM       average number of rooms per dwelling',
+    'AGE      proportion of owner-occupied units built prior to 1940',
+    'DIS      weighted distances to five Boston employment centres',
+    'RAD      index of accessibility to radial highways',
+    'TAX      full-value property-tax rate per $10,000',
+    'PTRATIO  pupil-teacher ratio by town',
+    'B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town',
+    'LSTAT    % lower status of the population',
+    'MEDV     Median value of owner-occupied homes in $1000\'s'
+]
+
+# print(Xy[:,-1].min(), Xy[:,-1].mean(), Xy[:,-1].max())
+
+for i in range(Xy.shape[1]):
+    abbrv = titles[i].split('   ')[0]
+    label = titles[i].split('   ')[-1]
+    print(abbrv, label)
+    plt.title(abbrv)
+    plt.xlabel(label)
+    plt.ylabel('Median value of over-occupied homes in $1000\'s')
+    plt.plot(Xy[:,i], Xy[:,-1], 'ko', alpha=0.5)
+    plt.tight_layout()
+    plt.show()
+
+# Question 4.1 RM (column 5) and LSTAT (column 12) are the most correlated.
+# Xy = np.column_stack((Xy[:, 5], Xy[:, 12], np.ones(shape=Xy.shape[0]), Xy[:, -1]))
+# print(Xy[:20])
+# test = Xy[-100:]
+# train = Xy[:-100]
+# X, y = train[:, :-1], train[:, -1]
+# _X, _y = test[:, :-1], test[:, -1]
+
+# 4.2 
+# linear regression model
+# beta = np.linalg.inv(X.T @ X) @ X.T @ y
+
+# root mean squared error (rmse) 
+# J = (_X @ beta - _y).T @ (_X @ beta - _y)
+# rmse = np.sqrt(J / _X.shape[0])
+# print(rmse)
