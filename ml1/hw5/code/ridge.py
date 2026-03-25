fdb04291899b20e3102e4ea600be025182b97ed6
bpmryan <bpm_ryand2428@gmail.com>
2026-03-25 16:01:23 -0500

    Configure and add solution to ridge.py

--- ml1/hw5/code/ridge.py
+++ ml1/hw5/code/ridge.py
@@ -1,7 +1,45 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('TkAgg') 
+import matplotlib.pyplot as plt
 
-RNG = np.random.default_rng(42)
+PATH = '../media/BostonHousing.csv'
+
+Xy = np.genfromtxt(
+    PATH, 
+    delimiter=',',
+    skip_header=1,
+    dtype=np.float64,
+    converters = {3: lambda s: float(s[1: -1])}
+)
+
+# Question 4.1 RM (column 5) and LSTAT (column 12) are the most correlated.
+Xy = np.column_stack((Xy[:, 5], Xy[:, 12], np.ones(shape=Xy.shape[0]), Xy[:, -1]))
+print(Xy[:20])
+test = Xy[-100:]
+train = Xy[:-100]
+X, y = train[:, :-1], train[:, -1]
+_X, _y = test[:, :-1], test[:, -1]
+
+def ridge_beta(X, y,  lmb):
+    p = X.shape[1]
+    I = np.eye(p)
+    return np.linalg.inv(X.T @ X + lmb * I) @ X.T @ y
+
+def rmse(X, y, lmd):
+    beta = ridge_beta(X, y, lmd) 
+    y_pred = X @ beta
+    return np.sqrt(np.mean((y - y_pred) ** 2))
 
 # lambda
-lmb = RNG.integers(-10, 20)
+lmb = np.arange(-10, 20)
+errors = [rmse(_X, _y, l) for l in  lmb]
+
+plt.plot(lmb, errors)
+plt.xlabel('λ')
+plt.ylabel('RMSE')
+plt.title('Ridge Regression RMSE vs λ')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('../media/ridgePlot.png')
+plt.show()
