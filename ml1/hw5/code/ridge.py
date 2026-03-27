7648c4c415dc5ea46407e5d34d6393e44b874a67
bpmryan <bpm_ryand2428@gmail.com>
2026-03-27 11:13:13 -0500

    Adjust solutions to question 2 and 3

--- ml1/hw5/code/ridge.py
+++ ml1/hw5/code/ridge.py
@@ -15,7 +15,6 @@ Xy = np.genfromtxt(
 
 # Question 4.1 RM (column 5) and LSTAT (column 12) are the most correlated.
 Xy = np.column_stack((Xy[:, 5], Xy[:, 12], np.ones(shape=Xy.shape[0]), Xy[:, -1]))
-print(Xy[:20])
 test = Xy[-100:]
 train = Xy[:-100]
 X, y = train[:, :-1], train[:, -1]
@@ -23,24 +22,30 @@ _X, _y = test[:, :-1], test[:, -1]
 
 def ridge_beta(X, y,  lmb):
     p = X.shape[1]
-    A = X.T @ X + lmb * np.eye(p)
-    B = X.T @ y
-    return np.linalg.solve(A, B)
+    I = np.eye(p)
+    return np.linalg.inv(X.T @ X + lmb * I) @ X.T @ y
 
-def rmse(X, y, lmd):
-    beta = ridge_beta(X, y, lmd) 
-    y_pred = X @ beta
-    return np.sqrt(np.mean((y - y_pred) ** 2))
+def rmse(X, y, _X, _y, lmb):
+    beta = ridge_beta(X, y, lmb) 
+    y_pred = _X @ beta
+    return np.sqrt(np.mean((_y - y_pred) ** 2))
 
 # lambda
 lmb = np.arange(-10, 20)
-errors = [rmse(_X, _y, l) for l in  lmb]
+errors = [rmse(X, y, _X, _y, l) for l in  lmb]
+
+# Answer to question 3
+best_lmb = lmb[np.argmin(errors)]
+best_rmse = min(errors)
+print(f"Best λ: {best_lmb}, RMSE: {best_rmse:.4f}")
 
 plt.plot(lmb, errors)
 plt.xlabel('λ')
 plt.ylabel('RMSE')
 plt.title('Ridge Regression RMSE vs λ')
+plt.xticks(np.arange(-10, 20, 2))
 plt.grid(True)
 plt.tight_layout()
 plt.savefig('../media/ridgePlot.png')
 plt.show()
+
